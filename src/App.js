25c97cfdeef6c7ca8301194d49761e68d603d85c
Neema-git <neemasrivastava05@gmail.com>
2023-03-31 01:01:14 +0530

    add changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,183 @@
-import logo from './logo.svg';
 import './App.css';
+import { React, useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import JSONtoTable from './JSONtoTable';
+import Spinner from './Spinner';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 function App() {
+
+  const [data,setData]=useState([]);
+  const [isloading, setIsLoading] = useState(false);
+  const [jsonToTableData, setJsonToTableData] = useState([]);
+  const [plotImg, setPlotImg] = useState([]);
+
+  const getData=()=>{
+    fetch('./data.json'
+    ,{
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson)
+      });
+  }
+
+  useEffect(()=>{
+    getData()
+  },[])
+
+  function handleClick(){
+    setIsLoading(true);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+      };
+  fetch('http://54.152.131.225:5000/evaluate', requestOptions)
+      .then(response => response.json())
+      .then((text) => {
+        setJsonToTableData(text.data);
+        setPlotImg(text.pltImg)
+        setIsLoading(false);
+       
+        let res = document.getElementById("flask-resp");
+        let resimg = document.getElementById("flask-resp-img");
+
+        console.log(res);
+        
+        if(res.style.display === "none")
+        {
+          res.style.display = "flex";
+          resimg.style.display = "flex";
+        }
+        else
+        {
+          res.style.display = "none";
+          resimg.style.display = "none";
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        setIsLoading(false);
+    });
+  }
+  
+  function showStaticData() {
+    let table = document.getElementById("static-data-table");
+    if (table.style.display === "none") {
+      table.style.display = "table";
+    } else {
+      table.style.display = "none";
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div style={{backgroundColor: "darkblue"}}>
+        <h1 style={{textAlign: "center", color: "white"}}>Derivative Pricing</h1>
+      </div>
+      <div className='section1' style={{display: "flex", flexDirection: "column"}}>
+        <div className="sec1headerwrapper" style={{display: "flex", flexDirection: "row",alignItems: "center"}}>
+          <div>
+            <h4 style={{paddingLeft: "10px"}}>TRAINED MODEL SUMMARY</h4>
+          </div> 
+        </div>
+        
+        <div style={{alignSelf: "center"}}>
+          {
+            <img style={{width: "500px", margin: "10px"}} id='plot-image' src='https://marketsworkshop.s3.amazonaws.com/mldata/dataplot.png' alt=''>
+
+            </img>}
+        </div>
+      </div>
+
+      <div className='section2' style={{display: "flex", flexDirection: "column"}}>
+        <div className='sec2headerwrapper' style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+        <h4 style={{paddingLeft: "10px"}}>TEST DATA FOR PRICE PREDICTION</h4>
+          <div style={{margin: "10px"}}>
+              <Button onClick={showStaticData} variant="contained" color="secondary">
+                View Test Data
+              </Button>
+            </div>
+          </div>
+              <TableContainer component={Paper} style={{maxWidth: "99%", margin: "10px", overflowX: "auto"}}>
+                <Table id="static-data-table" style={{display: "none"}} sx={{ minWidth: 700 }} aria-label="customized table">
+                  <TableHead>
+                    <TableRow>
+                      {data.length > 0 &&
+                        Object.keys(data[0]).map((key) => <StyledTableCell key={key} style={{backgroundColor: "black", color: "white"}}>{key.toUpperCase()}</StyledTableCell>)}
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.map((row, index) => ( 
+                      <StyledTableRow key={index}>
+                        {Object.values(row).map((value, index) => (
+                          <StyledTableCell  key={index} style={{whiteSpace: "nowrap"}}>{value}</StyledTableCell>
+                        ))}
+                      </StyledTableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+        </div>
+
+        <div className='section3'>
+          <div className='sec3headerwrapper' style={{display: "flex", flexDirection: "row", alignItems: "center"}}>
+
+            <h4 style={{paddingLeft: "10px"}}>PREDICT PRICING WITH TEST DATA</h4>
+            <div style={{margin: "10px"}}>            
+              <Button onClick={handleClick} disabled={isloading} variant="contained" color="primary" component="span">
+                Predict Price
+              </Button>
+            </div>
+            </div>
+
+          {isloading ? <Spinner/> : 
+            <div style={{display: "flex", flexDirection: "row"}}>
+              <div id='flask-resp' style={{flex:"0 0 50%",padding: "5% 2% 0 0",alignItems: "center"}}>
+
+              {jsonToTableData.length > 0 ? <JSONtoTable data={jsonToTableData} /> : false}
+              </div>
+              <div id='flask-resp-img' style={{flex:"0 0 50%",padding: "2%",alignItems: "right"}}>
+                <img src={plotImg} alt='' style={{width: "500px"}}></img>  {/* hardcoded image link */}
+              </div>
+            </div>
+          }
+            
+        </div>
     </div>
   );
 }
---
+++ src/JSONtoTable.js
@@ -0,0 +1,71 @@
+// import {useMemo} from 'react';
+import { Table, TableBody, TableContainer, TableHead, TableRow, Paper } from '@material-ui/core';
+import { styled } from '@mui/material/styles';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+
+function JSONtoTable({data}) {
+
+    // const columns = useMemo(() => [                            // useMemo to prevent re-renders of useTable
+    //     { Header: 'InstrumentId', accessor: 'instrumentId' },
+    //     { Header: 'Actual', accessor: 'actual' },
+    //     { Header: 'Predicted', accessor: 'predicted' },
+    //     { Header: 'Diff', accessor: 'diff' },
+    //   ],[]);
+    
+    // const tablefromJSON = useTable({ columns, data });
+
+    return (
+    <TableContainer component={Paper} style={{margin: "10px", overflowX: "auto", height: "fit-content", alignSelf: "center"}}>
+    <Table> 
+      <TableHead>
+        <StyledTableRow>
+        {/* {
+            ['instrumentId','actual','predicted','diff'].forEach((x) =>
+                {<TableCell style={{backgroundColor: "black", color: "white"}}>{x.toUpperCase()}</TableCell>}
+            )
+        } */}
+          <StyledTableCell style={{backgroundColor: "black", color: "white"}}>INSTRUMENTID</StyledTableCell>
+          <StyledTableCell style={{backgroundColor: "black", color: "white"}}>ACTUAL</StyledTableCell>
+          <StyledTableCell style={{backgroundColor: "black", color: "white"}}>PREDICTED</StyledTableCell>
+          <StyledTableCell style={{backgroundColor: "black", color: "white"}}>DIFF</StyledTableCell>
+        </StyledTableRow>
+      </TableHead>
+      <TableBody>
+        {data.map((item) => (
+          <StyledTableRow key={item.instrumentId}>
+            <StyledTableCell>{item.instrumentId}</StyledTableCell>
+            <StyledTableCell>{item.actual}</StyledTableCell>
+            <StyledTableCell>{item.predicted}</StyledTableCell>
+            <StyledTableCell>{item.diff}</StyledTableCell>
+          </StyledTableRow>
+        ))}
+      </TableBody>
+    </Table>
+    </TableContainer>
+
+  
+    );
+};
+
+export default JSONtoTable;
